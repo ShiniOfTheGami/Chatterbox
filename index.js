ebc9ffc235df04950f5d39c336f323fbccc79532
psanker <patricksanker@gmail.com>
2015-02-15 17:27:10 -0500

    Switching over

---
+++ index.js
@@ -0,0 +1,1 @@
+// Boot script for chat server -- redirects to the main boot scripts
---
+++ src/chatterbox-server.js
@@ -0,0 +1,13 @@
+function Chatterbox(rootApp) {
+	this.rootApp = rootApp;
+}
+
+Chatterbox.prototype.bootServer = function() {
+	// Keep a copy of the active class
+	var self    = this,
+			rootApp = self.rootApp;
+};
+
+Chatterbox.prototype.stopServer = function() {
+	// body...
+};
