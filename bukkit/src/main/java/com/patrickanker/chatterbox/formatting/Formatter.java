8b5e5aca1431a54c94a461c16db9707ed0cec964
psanker <patricksanker@gmail.com>
2015-02-15 17:36:01 -0500

    Forgot this stuff

---
+++ bukkit/src/main/java/com/patrickanker/chatterbox/formatting/Formatter.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2015 Patrick Anker and contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *  
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.patrickanker.chatterbox.formatting;
+
+import com.patrickanker.chatterbox.messenger.Messenger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+public abstract class Formatter {
+
+  private static final String colourRegex = "&[A-Fa-fK-Ok-oRr0-9]";
+  
+  public abstract String format(String message, Messenger messenger, String targetUUID);
+  
+  public static String colourise(String input) {
+    String colourised = input;
+    
+    Pattern pattern = Pattern.compile(colourRegex);
+    Matcher match = pattern.matcher(colourised);
+    
+    while (match.find()) {
+      String found = match.group();
+      found = found.replace("&", "§");
+      colourised = match.replaceAll(found);
+    }
+    
+    return colourised;
+  }
+}
